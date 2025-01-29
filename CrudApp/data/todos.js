944c446a049c23e7c3fdbcb9d64a4f5deee75170
blasko-balazs-levente <114399916+blasko-balazs-levente@users.noreply.github.com>
2025-01-29 21:10:24 +0100

    Create todos.js

---
+++ CrudApp/data/todos.js
@@ -0,0 +1,102 @@
+export const data = [
+    {
+        "id": 1,
+        "title": "Finish reading the book",
+        "completed": true
+    },
+    {
+        "id": 2,
+        "title": "Write a blog post",
+        "completed": false
+    },
+    {
+        "id": 3,
+        "title": "Complete coding assignment",
+        "completed": true
+    },
+    {
+        "id": 4,
+        "title": "Go grocery shopping",
+        "completed": false
+    },
+    {
+        "id": 5,
+        "title": "Prepare dinner",
+        "completed": true
+    },
+    {
+        "id": 6,
+        "title": "Attend team meeting",
+        "completed": false
+    },
+    {
+        "id": 7,
+        "title": "Walk the dog",
+        "completed": true
+    },
+    {
+        "id": 8,
+        "title": "Pay electricity bill",
+        "completed": false
+    },
+    {
+        "id": 9,
+        "title": "Plan weekend trip",
+        "completed": false
+    },
+    {
+        "id": 10,
+        "title": "Call mom",
+        "completed": true
+    },
+    {
+        "id": 11,
+        "title": "Watch a documentary",
+        "completed": false
+    },
+    {
+        "id": 12,
+        "title": "Clean the house",
+        "completed": true
+    },
+    {
+        "id": 13,
+        "title": "Organize desk",
+        "completed": false
+    },
+    {
+        "id": 14,
+        "title": "Meditate for 15 minutes",
+        "completed": true
+    },
+    {
+        "id": 15,
+        "title": "Send an email to the client",
+        "completed": false
+    },
+    {
+        "id": 16,
+        "title": "Finish watching the series",
+        "completed": true
+    },
+    {
+        "id": 17,
+        "title": "Research for the project",
+        "completed": false
+    },
+    {
+        "id": 18,
+        "title": "Repair the leaking faucet",
+        "completed": true
+    },
+    {
+        "id": 19,
+        "title": "Prepare a presentation",
+        "completed": false
+    },
+    {
+        "id": 20,
+        "title": "Read an article on productivity",
+        "completed": true
+    }
+];
